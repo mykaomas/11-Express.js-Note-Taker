3b90c0521324fcc0790d938171b7d0dd3a35b7bd
matronae <mykaomas@gmail.com>
2024-02-05 22:21:14 -0600

    added routes

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+  
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+  });
+  
+  app.get('/api/notes', (req, res) => {
+    fs.readFile('./db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+        return;
+      }
+      res.json(JSON.parse(data));
+    });
+  });
+  
+
 app.listen(PORT, () =>
   console.log(`Listening for requests on port ${PORT}!`)
 );
