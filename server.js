de8a4c2a08bdb85ef889d40792bbcb0074fc0ea1
matronae <mykaomas@gmail.com>
2024-02-05 19:15:13 -0600

    changed layout of folders

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
